ede18ae0d20f9f5bdb39c682b9384f2dad8c4c82
alifateh <alifatehchehr@gmail.com>
2025-04-29 15:29:35 +0100

    change the PDF fle name

--- Autoapp/PWA/PDFGenerator.php
+++ Autoapp/PWA/PDFGenerator.php
@@ -259,8 +259,9 @@ $pdf->WriteHTML($cpoy_write, true, 0, true, 0);
 // ---------------------------------------------------------
 
 //Close and output PDF document
-$pdf->Output($tariff_auto[2] . '-' . $ver . '.pdf', 'I');
-
+// $pdf->Output($tariff_auto[2] . '-' . $ver . '.pdf', 'I');
+$pdf->Output(md5($codeContents) . '-' . md5($codeContents) . '.pdf', 'I');
+ 
 //============================================================+
 // END OF FILE
 //============================================================+
